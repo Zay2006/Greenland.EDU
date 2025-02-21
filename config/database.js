4f94782540fa572c0adf1da6b1d1dcea1dfa8fd3
Zay2006 <iwrig0048@launchpadphilly.org>
2025-02-21 18:33:03 +0000

    asdf

--- config/database.js
+++ config/database.js
@@ -4,7 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = () => {
-  const connection = mysql.createConnection(process.env.MYSQL_URI);
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
   connection.connect((err) => {
     if (err) {
