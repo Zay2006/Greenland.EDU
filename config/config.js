e43b37793eac82cffd3a09f9e42fdc019719a357
Zay2006 <iwrig0048@launchpadphilly.org>
2025-02-21 19:14:33 +0000

    Hello

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 export default {
   port: process.env.PORT || 3000,
-  dbName: process.env.DB_NAME || "world",
+  dbName: process.env.DB_NAME || "Greenfield.edu",
   dbUser: process.env.DB_USER || "root",
-  dbPassword: process.env.DB_PASSWORD || "",
+  dbPassword: process.env.DB_PASSWORD || "0tNyIZGhtpb9i1x",
   dbHost: process.env.DB_HOST || "localhost",
   env: process.env.NODE_ENV || "development",
 };
