9723243914a9ffb68dd149279482c094aed40b2f
Rob T. <techeducation@launchpadphilly.org>
2025-02-28 12:57:20 -0500

    updated server.js port

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(errorHandler);
 // Start the server
 const startServer = async () => {
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
   });
